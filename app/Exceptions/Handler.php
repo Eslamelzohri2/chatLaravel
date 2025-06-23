9dae0c4fe068913912e58c2ccdf490aa227480cb
Eslam Adel <elzohrioption@gmail.com>
2025-06-23 21:01:36 +0300

    رسالة التعديل

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -45,4 +46,9 @@ class Handler extends ExceptionHandler
             //
         });
     }
+   protected function unauthenticated($request, AuthenticationException $exception)
+{
+    return response()->json(['message' => 'Unauthorized'], 401);
+}
+    
 }
